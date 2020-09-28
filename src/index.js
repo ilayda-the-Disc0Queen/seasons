5ff4f11a6600624051e95ff4ab82ee0817dbbc73
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-09-28 18:55:47 +0100

    add loading spinner

--- src/index.js
+++ src/index.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 class App extends React.Component {
   // babel changes the line below into the component method we had before
   state = { lat: null, errorMessage: '' };
 
-componentDidMount() {
-  window.navigator.geolocation.getCurrentPosition(
-    position => this.setState({ lat: position.coords.latitude }),
-    err => this.setState({ errorMessage: err.message })
-    );
-}
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+      );
+  }
 
-componentDidUpdate() {
-  console.log('My component was just updated - it rerendered!')
-}
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!')
+  }
 
-  render() {
-    // render gets called A LOT so we took the geolocation stuff
-    // out so it doesn't slow down the page
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading! Please wait...</div>;
+    return <Loader loadingMessage="Please accept location request" />;
+  }
 
-  };
-};
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
 
 ReactDOM.render(
   <App />, document.querySelector('#root')
