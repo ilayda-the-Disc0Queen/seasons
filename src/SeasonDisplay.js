2bc6fc8c49b41cc9e3df5d807142dba839c860b2
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-09-28 11:24:35 +0100

    add background and icon colours for both seasons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -25,10 +26,12 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const { text, iconName } = seasonConfig[season]; // returns
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    // make outer div className (season-display) match the
+    // component it's tied to (SeasonDisplay)
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
     );
 };
